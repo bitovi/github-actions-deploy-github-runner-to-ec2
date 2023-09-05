01a8a428648ff54b37f1b0bea4db68c28b4e9930
LeoDiazL <116020815+LeoDiazL@users.noreply.github.com>
2023-09-05 12:37:19 -0300

    user-data.sh fix

--- user-data.sh
+++ user-data.sh
@@ -57,4 +57,4 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin 
